bbd9861538eb5f8bc3340b57ef76d3a0d4f27bef
Pape Makhtar Gueye <pgueye@tomware.it>
2024-02-22 11:19:38 +0000

    first commit

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "lunarvim/synthwave84.nvim",
+    lazy = true,
+    priority = 1000,
+    -- opts = function()
+    --   return {
+    --     transparent = true,
+    --   }
+    -- end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "synthwave84",
+    },
+  },
+}
